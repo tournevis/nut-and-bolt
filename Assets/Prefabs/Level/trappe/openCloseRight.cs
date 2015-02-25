fbdd72082088871e6f1dabf0a814a2f34753fc7c
Brossault <brossault.leo@gmail.com>
2015-02-25 15:31:10 +0100

    Leo's work

---
+++ Assets/Prefabs/Level/trappe/openCloseRight.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class openCloseRight : MonoBehaviour {
+
+	public float aleaTimeOpen = 0;
+	public float aleaTimeClose = 3;
+	
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		aleaTimeOpen -= Time.deltaTime; 
+//		
+//		if(aleaTimeOpen <= 0) {
+//			openTrap();
+//			aleaTimeClose -= Time.deltaTime; 
+//			
+//			if(aleaTimeClose <= 0) {
+//				closeTrap();
+//			}
+//		}
+		
+	}
+	
+	public void openTrap () {
+		transform.GetComponent<Animator> ().SetBool ("trapRight", true);
+	}
+	
+	public void closeTrap () {
+		Debug.Log ("Test");
+		transform.GetComponent<Animator> ().SetBool ("trapRightClose", true);
+	}
+}

929de465e567da6e9223a549f382fc6f1fdc03bb
Valentin Daguenet <valentin.daguenet@gmail.com>
2015-02-25 15:44:49 +0100

    Add rotation

--- Assets/Scripts/Bonus/BonusTrigger.cs
+++ Assets/Scripts/Bonus/BonusTrigger.cs
@@ -18,7 +18,7 @@ public class BonusTrigger : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		transform.Rotate (0.0f, 35.0f * Time.deltaTime, 0.0f);
 	}
 
 	void OnTriggerEnter (Collider col) {
--- Assets/Scripts/Weapon/WeaponTrigger.cs
+++ Assets/Scripts/Weapon/WeaponTrigger.cs
@@ -16,7 +16,7 @@ public class WeaponTrigger : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		transform.Rotate (0.0f, 35.0f * Time.deltaTime, 0.0f);
 	}
 
 	void OnTriggerEnter (Collider col) {

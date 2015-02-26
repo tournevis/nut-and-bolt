4b0e567c1a9e776124df500b491ccf71dda0afee
Brossault <brossault.leo@gmail.com>
2015-02-26 17:38:54 +0100

    joystick

---
+++ Assets/Scripts/directionShoot.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class directionShoot : MonoBehaviour {
+	PlayerRobot robotParent;
+	private float viewV;
+	private float viewH;
+
+	// Use this for initialization
+	void Start () {
+		robotParent = transform.parent.GetComponent<PlayerRobot> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (robotParent.id == 0) {
+			viewV = Input.GetAxisRaw("VerticalView1");
+			viewH = Input.GetAxisRaw("HorizontalView1");
+		} else if (robotParent.id == 1) {
+
+		} else {
+
+		}
+
+		transform.Rotate (0, viewH, 0);
+	}
+}
